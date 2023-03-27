e7265a2c75b6eeafcf78a70e7e89b28d7ec6a54d
Quin Zubella <quinzubella@Quins-MacBook-Pro.local>
2023-03-27 16:08:42 -0500

    added Formik

--- src/components/auth/BadgerLogin.js
+++ src/components/auth/BadgerLogin.js
@@ -3,6 +3,7 @@ import { Button } from "react-bootstrap";
 import { useNavigate } from 'react-router-dom';
 import LoginContext from '../../contexts/LoginContext';
 import UserContext from '../../contexts/UserContext';
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 export default function BadgerLogin() {
 
@@ -16,62 +17,86 @@ export default function BadgerLogin() {
         navigate('/');
     }
 
-    const login2 = () => {
-        if(inputUser.current.value.length < 1 || inputPass.current.value.length <1){
-            alert("You must provide both a username and password!");
-            return;
+const submit = (values, { setSubmitting }) => {
+    fetch("https://cs571.org/s23/hw6/api/login", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CS571-ID": "bid_0bf7690d166cd6659c0f",
+      },
+      body: JSON.stringify({
+        username: values.username,
+        password: values.password,
+      }),
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          setUser(values.username);
+          setLogin(true);
+          alert("Login Successful");
+          handle();
+          return res.json();
+        } else if (res.status === 404) {
+          alert("Username does not exist");
+        } else if (res.status === 401) {
+          alert("Password is incorrect");
+        } else {
+          throw new Error();
         }
-      
-        else {
-            fetch('https://cs571.org/s23/hw6/api/login', {
-                method: 'POST',
-                credentials: 'include',
-                headers: {
-                    "Content-Type": "application/json",
-                    "X-CS571-ID": "bid_0bf7690d166cd6659c0f"
-                },
-                body: JSON.stringify({
-                    username: inputUser.current.value,
-                    password: inputPass.current.value
-                })
-            }).then(res => {
-                if (res.status === 200) {
-                    setUser(inputUser.current.value);
-                    setLogin(true);
-                    alert('Login Successful')
-                    handle();
-                    return res.json();
-                } 
-                else if (res.status === 404) {
-                    alert('Username does not exist')
-                }
-                else if (res.status === 401) {
-                    alert('Password is incorrect')
-                }else {
-                    throw new Error()
-                }
-            }).then(json => {
-                
-                
-            }).catch(e => {
-                alert('That username has already been taken!')
-            })
-        }
-    }
+      })
+      .then((json) => {})
+      .catch((e) => {
+        alert("That username has already been taken!");
+      })
+      .finally(() => {
+        setSubmitting(false);
+      });
+  };
 
-    return <>
-        <h1>Login</h1>
-        <form>
-                <label htmlFor="username">Username</label>
-                <div/>
-                <input id="title" ref={inputUser}/>
-                <div/>
-                <label htmlFor="password">Password</label>
-                <div/>
-                <input id="password" type="password" ref={inputPass}/>
-                <div/>
-                
-        </form>
-        <Button onClick={login2} style={{marginTop: "1rem"}}>Login</Button>
+  const validate = (values) => {
+    const errors = {};
+    if (!values.username) {
+      errors.username = "Username is required";
+    }
+    if (!values.password) {
+      errors.password = "Password is required";
+    }
+    return errors;
+  };
+
+  return (
+    <>
+      <h1>Login</h1>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        validate={validate}
+        onSubmit={submit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div>
+              <label htmlFor="username">Username</label>
+              <Field id="username" name="username" type="text" />
+              <ErrorMessage name="username" component="div" />
+            </div>
+
+            <div>
+              <label htmlFor="password">Password</label>
+              <Field id="password" name="password" type="password" />
+              <ErrorMessage name="password" component="div" />
+            </div>
+
+            <Button
+              type="submit"
+              disabled={isSubmitting}
+              style={{ marginTop: "1rem" }}
+            >
+              Login
+            </Button>
+          </Form>
+        )}
+      </Formik>
     </>
+  );
 }
