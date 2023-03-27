5ad046b86c5538db4134ff5ead81b7637ea81684
Quin Zubella <quinzubella@Quins-MacBook-Pro.local>
2023-03-27 16:27:05 -0500

    finished

--- src/components/auth/BadgerLogin.js
+++ src/components/auth/BadgerLogin.js
@@ -20,7 +20,7 @@ export default function BadgerLogin() {
 const submit = (values, { setSubmitting }) => {
     fetch("https://cs571.org/s23/hw6/api/login", {
       method: "POST",
-      credentials: "include",
+       credentials: "include",
       headers: {
         "Content-Type": "application/json",
         "X-CS571-ID": "bid_0bf7690d166cd6659c0f",
@@ -30,13 +30,13 @@ const submit = (values, { setSubmitting }) => {
         password: values.password,
       }),
     })
-      .then((res) => {
+       .then((res) => {
         if (res.status === 200) {
           setUser(values.username);
-          setLogin(true);
+            setLogin(true);
           alert("Login Successful");
           handle();
-          return res.json();
+           return res.json();
         } else if (res.status === 404) {
           alert("Username does not exist");
         } else if (res.status === 401) {
@@ -45,23 +45,21 @@ const submit = (values, { setSubmitting }) => {
           throw new Error();
         }
       })
-      .then((json) => {})
-      .catch((e) => {
-        alert("That username has already been taken!");
-      })
-      .finally(() => {
-        setSubmitting(false);
-      });
-  };
+        .then((json) => {})
+        .catch((e) => {
+            alert("That username has already been taken!");
+        })
+        .finally(() => {
+          setSubmitting(false);
+          });
+    };
 
   const validate = (values) => {
     const errors = {};
     if (!values.username) {
-      errors.username = "Username is required";
-    }
-    if (!values.password) {
-      errors.password = "Password is required";
-    }
+      errors.username = "Username is required";}
+      if (!values.password) {
+       errors.password = "Password is required"; }
     return errors;
   };
 
@@ -71,30 +69,28 @@ const submit = (values, { setSubmitting }) => {
       <Formik
         initialValues={{ username: "", password: "" }}
         validate={validate}
-        onSubmit={submit}
-      >
+        onSubmit={submit}>
         {({ isSubmitting }) => (
-          <Form>
-            <div>
-              <label htmlFor="username">Username</label>
-              <Field id="username" name="username" type="text" />
-              <ErrorMessage name="username" component="div" />
-            </div>
 
-            <div>
-              <label htmlFor="password">Password</label>
-              <Field id="password" name="password" type="password" />
-              <ErrorMessage name="password" component="div" />
-            </div>
+        <Form>
+          <div>
+            <label htmlFor="username">Username</label>
+            <Field id="username" name="username" type="text" />
+            <ErrorMessage name="username" component="div" />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <Field id="password" name="password" type="password" />
+          <ErrorMessage name="password" component="div" />
 
-            <Button
-              type="submit"
-              disabled={isSubmitting}
-              style={{ marginTop: "1rem" }}
-            >
-              Login
-            </Button>
-          </Form>
+          </div>
+          <Button
+            type="submit"
+            disabled={isSubmitting}
+            style={{ marginTop: "1rem" }}>
+            Login
+          </Button>
+        </Form>
         )}
       </Formik>
     </>
